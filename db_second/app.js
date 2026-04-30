45a74958180ca5d20553a7802af44102e968bbd9
awais9845 <waqarkhattak844@gmail.com>
2026-04-30 04:59:45 -0700

    studied comparison operator syntax and practically applied

--- db_second/app.js
+++ db_second/app.js
@@ -3,6 +3,7 @@ import { config } from "dotenv";
 import mongoose from "mongoose";
 import { connectiondb } from "./connection/conn.js";
 import routes from "./routes/user.js";
+import { user } from "./models/user.js";
 config();
 const app = express();
 app.use(express.json());
@@ -27,6 +28,34 @@ connectiondb();
 // });
 
 app.use("/api", routes);
+
+app.get("/user", async (req, res) => {
+  try {
+    // const findUser = await user.find({ age: { $gt: 18 } });
+    // const findUser = await user.find({ age: { $eq: 18 } });
+    // const findUser = await user.find({ age: { $ne: 18 } });
+    // const findUser = await user.find({ age: { $lt: 18 } });
+    const findUser = await user.find({
+      $and: [{ age: { $lt: 30 } }, { email: "askj,nfjalkd23@gmail.com" }],
+    });
+    console.log(findUser);
+    if (!findUser) {
+      return res.status(404).json({
+        message: "user not found",
+        success: false,
+      });
+    }
+    // res.send("working");
+
+    res.status(200).json({
+      message: "required user:",
+      success: true,
+      findUser,
+    });
+  } catch (error) {
+    console.log("showing errror", error.message);
+  }
+});
 
 app.listen(1000, () => {
   console.log("http://localhost:1000");
--- db_second/connection/conn.js
+++ db_second/connection/conn.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectiondb = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb://waqarkhattak844_db_user:qXAqoj4HcNL8McEn@ac-bxtljjc-shard-00-00.gyrr6ni.mongodb.net:27017,ac-bxtljjc-shard-00-01.gyrr6ni.mongodb.net:27017,ac-bxtljjc-shard-00-02.gyrr6ni.mongodb.net:27017/?ssl=true&replicaSet=atlas-21ewq2-shard-0&authSource=admin&appName=Cluster0",
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log("db is working ");
   } catch (error) {
     console.log("db not working", error.message);
